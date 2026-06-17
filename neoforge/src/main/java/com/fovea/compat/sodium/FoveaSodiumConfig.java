35daaf5eb53c2f7efeba8cf64e53319e8d7b6978
blank <rqshankleton@gmail.com>
2026-06-17 17:47:35 -0400

    Add Sodium settings page integration

---
+++ neoforge/src/main/java/com/fovea/compat/sodium/FoveaSodiumConfig.java
@@ -0,0 +1,27 @@
+package com.fovea.compat.sodium;
+
+import com.fovea.Constants;
+import net.caffeinemc.mods.sodium.api.config.ConfigEntryPoint;
+import net.caffeinemc.mods.sodium.api.config.ConfigEntryPointForge;
+import net.caffeinemc.mods.sodium.api.config.structure.ConfigBuilder;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+
+@ConfigEntryPointForge(Constants.MOD_ID)
+public class FoveaSodiumConfig implements ConfigEntryPoint {
+
+    @Override
+    public void registerConfigLate(ConfigBuilder builder) {
+        builder.registerOwnModOptions()
+                .addPage(builder.createOptionPage()
+                        .setName(Component.literal(Constants.MOD_NAME))
+                        .addOptionGroup(builder.createOptionGroup()
+                                .addOption(builder.createExternalButtonOption(Identifier.parse(Constants.MOD_ID + ":log_button"))
+                                        .setName(Component.literal("Log Button"))
+                                        .setTooltip(Component.literal("Logs a message from Fovea."))
+                                        .setScreenConsumer(screen -> Constants.LOG.info("Fovea Sodium settings button clicked"))
+                                )
+                        )
+                );
+    }
+}
